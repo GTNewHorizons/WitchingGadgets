f3dc8dde8f4fcfd306a442c54e6bbb999d015bb0
Ethryan <3237986+Ethryan@users.noreply.github.com>
2024-06-15 20:42:11 +0200

    Fix missing recipes for eventual GT requirement removal

--- src/main/java/witchinggadgets/common/recipes/infusion/WG_infusion_terraformer.java
+++ src/main/java/witchinggadgets/common/recipes/infusion/WG_infusion_terraformer.java
@@ -1,6 +1,7 @@
 package witchinggadgets.common.recipes.infusion;
 
 import static witchinggadgets.common.recipes.WG_Infusion_recipes.registerInfusionRecipe;
+import static witchinggadgets.common.recipes.WG_arcane_recipes.registerArcaneRecipe;
 
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
@@ -22,20 +23,36 @@ import witchinggadgets.common.WGContent;
 public class WG_infusion_terraformer {
 
     public static void registerTerraformer() {
-        if (Loader.isModLoaded("dreamcraft")) {
-            registerInfusionRecipe(
-                    "TERRAFORMER",
-                    "",
-                    new ItemStack(WGContent.BlockMetalDevice, 1, 2),
-                    10,
-                    new AspectList().add(Aspect.WEATHER, 64).add(Aspect.EXCHANGE, 256)
-                            .add((Aspect) gregtech.api.enums.TC_Aspects.NEBRISUM.mAspect, 32),
-                    new ItemStack(ConfigBlocks.blockMetalDevice, 1, 9),
-                    new ItemStack[] { Materials.Blaze.getBlocks(1),
-                            GT_OreDictUnificator.get(OrePrefixes.block, Materials.Void, 1L),
-                            GT_ModHandler.getModItem("dreamcraft", "tile.CallistoColdIce", 1L),
-                            Materials.Knightmetal.getBlocks(1), gregtech.api.enums.ItemList.Field_Generator_MV.get(1L),
-                            GT_OreDictUnificator.get(OrePrefixes.block, Materials.Void, 1L) });
+        if (Loader.isModLoaded("gregtech")) {
+            if (Loader.isModLoaded("dreamcraft")) {
+                registerInfusionRecipe(
+                        "TERRAFORMER",
+                        "",
+                        new ItemStack(WGContent.BlockMetalDevice, 1, 2),
+                        10,
+                        new AspectList().add(Aspect.WEATHER, 64).add(Aspect.EXCHANGE, 256)
+                                .add((Aspect) gregtech.api.enums.TC_Aspects.NEBRISUM.mAspect, 32),
+                        new ItemStack(ConfigBlocks.blockMetalDevice, 1, 9),
+                        new ItemStack[]{Materials.Blaze.getBlocks(1),
+                                GT_OreDictUnificator.get(OrePrefixes.block, Materials.Void, 1L),
+                                GT_ModHandler.getModItem("dreamcraft", "tile.CallistoColdIce", 1L),
+                                Materials.Knightmetal.getBlocks(1), gregtech.api.enums.ItemList.Field_Generator_MV.get(1L),
+                                GT_OreDictUnificator.get(OrePrefixes.block, Materials.Void, 1L)});
+            } else {
+                registerInfusionRecipe(
+                        "TERRAFORMER",
+                        "",
+                        new ItemStack(WGContent.BlockMetalDevice, 1, 2),
+                        10,
+                        new AspectList().add(Aspect.WEATHER, 64).add(Aspect.EXCHANGE, 256)
+                                .add((Aspect) gregtech.api.enums.TC_Aspects.NEBRISUM.mAspect, 32),
+                        new ItemStack(ConfigBlocks.blockMetalDevice, 1, 9),
+                        new ItemStack[]{Materials.Blaze.getBlocks(1),
+                                GT_OreDictUnificator.get(OrePrefixes.block, Materials.Void, 1L),
+                                new ItemStack(Blocks.packed_ice),
+                                Materials.Knightmetal.getBlocks(1), gregtech.api.enums.ItemList.Field_Generator_MV.get(1L),
+                                GT_OreDictUnificator.get(OrePrefixes.block, Materials.Void, 1L)});
+            }
 
             registerInfusionRecipe(
                     "TERRAFORMFOCUS_TAINT",
@@ -250,6 +267,109 @@ public class WG_infusion_terraformer {
                             new ItemStack(Items.iron_ingot), new ItemStack(ConfigBlocks.blockTube, 1, 0),
                             new ItemStack(Items.iron_ingot), new ItemStack(Items.iron_ingot) });
             ThaumcraftApi.addWarpToItem(new ItemStack(WGContent.BlockMetalDevice, 1, 12), 2);
+
+            registerArcaneRecipe(
+                    "TERRAFORMFOCUS_PLAINS",
+                    "",
+                    new ItemStack(WGContent.BlockMetalDevice, 1, 3),
+                    new AspectList().add(Aspect.EARTH, 10),
+                    " S ",
+                    "IBI",
+                    "ITI",
+                    'B',
+                    new ItemStack(Blocks.grass),
+                    'I',
+                    "ingotIron",
+                    'S',
+                    new ItemStack(ConfigItems.itemShard, 1, 6),
+                    'T',
+                    new ItemStack(ConfigBlocks.blockTube));
+
+            registerArcaneRecipe(
+                    "TERRAFORMFOCUS_COLDTAIGA",
+                    "",
+                    new ItemStack(WGContent.BlockMetalDevice, 1, 4),
+                    new AspectList().add(Aspect.WATER, 10).add(Aspect.ORDER, 10),
+                    " S ",
+                    "IBI",
+                    "ITI",
+                    'B',
+                    new ItemStack(Blocks.ice),
+                    'I',
+                    "ingotIron",
+                    'S',
+                    new ItemStack(ConfigItems.itemShard, 1, 6),
+                    'T',
+                    new ItemStack(ConfigBlocks.blockTube));
+
+            registerArcaneRecipe(
+                    "TERRAFORMFOCUS_DESERT",
+                    "",
+                    new ItemStack(WGContent.BlockMetalDevice, 1, 5),
+                    new AspectList().add(Aspect.FIRE, 10).add(Aspect.EARTH, 10),
+                    " S ",
+                    "IBI",
+                    "ITI",
+                    'B',
+                    new ItemStack(Blocks.sand),
+                    'I',
+                    "ingotIron",
+                    'S',
+                    new ItemStack(ConfigItems.itemShard, 1, 6),
+                    'T',
+                    new ItemStack(ConfigBlocks.blockTube));
+
+            registerArcaneRecipe(
+                    "TERRAFORMFOCUS_JUNGLE",
+                    "",
+                    new ItemStack(WGContent.BlockMetalDevice, 1, 6),
+                    new AspectList().add(Aspect.WATER, 10).add(Aspect.EARTH, 10),
+                    " S ",
+                    "IBI",
+                    "ITI",
+                    'B',
+                    new ItemStack(Blocks.log, 1, 3),
+                    'I',
+                    "ingotIron",
+                    'S',
+                    new ItemStack(ConfigItems.itemShard, 1, 6),
+                    'T',
+                    new ItemStack(ConfigBlocks.blockTube));
+
+            registerArcaneRecipe(
+                    "TERRAFORMFOCUS_HELL",
+                    "",
+                    new ItemStack(WGContent.BlockMetalDevice, 1, 7),
+                    new AspectList().add(Aspect.FIRE, 10).add(Aspect.ENTROPY, 10),
+                    " S ",
+                    "IBI",
+                    "ITI",
+                    'B',
+                    new ItemStack(Blocks.nether_brick),
+                    'I',
+                    "ingotIron",
+                    'S',
+                    new ItemStack(ConfigItems.itemShard, 1, 6),
+                    'T',
+                    new ItemStack(ConfigBlocks.blockTube));
+            ThaumcraftApi.addWarpToItem(new ItemStack(WGContent.BlockMetalDevice, 1, 7), 1);
+
+            registerArcaneRecipe(
+                    "TERRAFORMFOCUS_MUSHROOM",
+                    "",
+                    new ItemStack(WGContent.BlockMetalDevice, 1, 9),
+                    new AspectList().add(Aspect.EARTH, 10).add(Aspect.WATER, 10),
+                    " S ",
+                    "IBI",
+                    "ITI",
+                    'B',
+                    new ItemStack(Blocks.mycelium),
+                    'I',
+                    "ingotIron",
+                    'S',
+                    new ItemStack(ConfigItems.itemShard, 1, 6),
+                    'T',
+                    new ItemStack(ConfigBlocks.blockTube));
         }
     }
 }
