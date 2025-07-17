e1cf7881f9b57e225439e4e4cbb4454c4316767f
Black Noise <smihaelk@gmail.com>
2025-07-17 18:51:35 +0300

    fix non-flying air control

--- src/main/java/witchinggadgets/common/items/armor/ItemPrimordialArmor.java
+++ src/main/java/witchinggadgets/common/items/armor/ItemPrimordialArmor.java
@@ -334,9 +334,9 @@ public class ItemPrimordialArmor extends ItemFortressArmor implements IPrimordia
                 }
                 player.jumpMovementFactor = 0.00002F;
             } else if (Hover.getHover(player.getEntityId())) {
-                player.jumpMovementFactor = 0.03F * speedMod;
+                player.jumpMovementFactor = 0.03F;
             } else {
-                player.jumpMovementFactor = 0.05F * speedMod;
+                player.jumpMovementFactor = 0.05F;
             }
         }
     }
