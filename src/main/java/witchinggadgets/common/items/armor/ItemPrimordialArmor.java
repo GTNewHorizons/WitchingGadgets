GTNewHorizons/WitchingGadgets

commit f55f23c093a38d047f85d4288d778163149d6c9c

makes omni mode boost vertical flight speed for primordial boots (#79)
Co-authored-by: koolkrafter5 <creepersrgreen@gmail.com>
Co-authored-by: Martin Robertz <dream-master@gmx.net>

src/main/java/witchinggadgets/common/items/armor/ItemPrimordialArmor.java
@@ -1,5 +1,7 @@
 package witchinggadgets.common.items.armor;
 
+import static taintedmagic.common.items.equipment.ItemVoidwalkerBoots.sashBuff;
+
 import java.util.List;
 
 import net.minecraft.block.material.Material;
@@ -37,6 +39,7 @@ import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import gregtech.api.hazards.Hazard;
 import gregtech.api.hazards.IHazardProtector;
+import taintedmagic.api.IVoidwalker;
 import thaumcraft.api.IRunicArmor;
 import thaumcraft.api.IVisDiscountGear;
 import thaumcraft.api.IWarpingGear;
@@ -50,14 +53,16 @@ import witchinggadgets.WitchingGadgets;
 import witchinggadgets.api.IPrimordialCrafting;
 import witchinggadgets.client.render.ModelPrimordialArmor;
 import witchinggadgets.common.WGContent;
+import witchinggadgets.common.WGModCompat;
 import witchinggadgets.common.items.interfaces.IItemEvent;
 import witchinggadgets.common.items.tools.IPrimordialGear;
 import witchinggadgets.common.util.Lib;
 
-@Optional.InterfaceList({ @Optional.Interface(iface = "thaumicboots.api.IBoots", modid = "thaumicboots"),
+@Optional.InterfaceList({ @Optional.Interface(iface = "taintedmagic.api.IVoidwalker", modid = "TaintedMagic"),
+        @Optional.Interface(iface = "thaumicboots.api.IBoots", modid = "thaumicboots"),
         @Optional.Interface(iface = "gregtech.api.hazards.IHazardProtector", modid = "gregtech_nh") })
 public class ItemPrimordialArmor extends ItemFortressArmor implements IPrimordialCrafting, IPrimordialGear, IRunicArmor,
-        IItemEvent, IBoots, IWarpingGear, IVisDiscountGear, IHazardProtector {
+        IItemEvent, IBoots, IWarpingGear, IVisDiscountGear, IHazardProtector, IVoidwalker {
 
     enum FlightStatus {
         ON,
@@ -303,11 +308,10 @@ public class ItemPrimordialArmor extends ItemFortressArmor implements IPrimordia
     }
 
     public void movementEffects(EntityPlayer player, float bonus, ItemStack itemStack) {
-        if (player.moveForward != 0.0F || player.moveStrafing != 0.0F) {
+        if (player.moveForward != 0.0F || player.moveStrafing != 0.0F || player.motionY != 0.0F) {
             if (WitchingGadgets.isBootsActive) {
                 boolean omniMode = isOmniEnabled(itemStack);
-                if ((player.moveForward == 0F && player.moveStrafing == 0F && omniMode)
-                        || (player.moveForward <= 0F && !omniMode)) {
+                if (player.moveForward <= 0F && !omniMode) {
                     return;
                 }
             }
@@ -320,6 +324,10 @@ public class ItemPrimordialArmor extends ItemFortressArmor implements IPrimordia
 
             float speedMod = (float) getSpeedModifier(itemStack);
             if (player.onGround || player.capabilities.isFlying || player.isOnLadder()) {
+
+                if (WGModCompat.loaded_TaintedMagic) {
+                    bonus += sashBuff(player);
+                }
                 bonus *= speedMod;
                 if (WitchingGadgets.isBootsActive) {
                     applyOmniState(player, bonus, itemStack);
@@ -331,10 +339,9 @@ public class ItemPrimordialArmor extends ItemFortressArmor implements IPrimordia
                 }
                 player.jumpMovementFactor = 0.00002F;
             } else if (Hover.getHover(player.getEntityId())) {
-                player.jumpMovementFactor = 0.03F * speedMod;
-
+                player.jumpMovementFactor = 0.03F;
             } else {
-                player.jumpMovementFactor = 0.05F * speedMod;
+                player.jumpMovementFactor = 0.05F;
             }
         }
     }
@@ -346,8 +353,17 @@ public class ItemPrimordialArmor extends ItemFortressArmor implements IPrimordia
         if (player.moveForward != 0.0) {
             player.moveFlying(0.0F, player.moveForward, bonus);
         }
-        if (player.moveStrafing != 0.0 && getOmniState(itemStack)) {
-            player.moveFlying(player.moveStrafing, 0.0F, bonus);
+        if (getOmniState(itemStack)) {
+            if (player.moveStrafing != 0.0) {
+                player.moveFlying(player.moveStrafing, 0.0F, bonus);
+            }
+            boolean jumping = Minecraft.getMinecraft().gameSettings.keyBindJump.getIsKeyPressed();
+            boolean sneaking = player.isSneaking();
+            if (sneaking && !jumping && !player.onGround) {
+                player.motionY -= bonus;
+            } else if (jumping && !sneaking) {
+                player.motionY += bonus;
+            }
         }
     }
 

dependencies.gradle
@@ -6,23 +6,23 @@ dependencies {
 
     implementation('com.github.GTNewHorizons:Baubles-Expanded:2.1.9-GTNH:dev')
     implementation('com.github.GTNewHorizons:ThaumicBases:1.8.11:dev') {transitive=false}
-    implementation('com.github.GTNewHorizons:Tainted-Magic:7.6.21-GTNH:dev') {
+    implementation('com.github.GTNewHorizons:Tainted-Magic:7.6.23-GTNH:dev') {
      //we don't want the old Baubles Messing stuff up so lets disable that
         exclude module: "Baubles"
     }
-    implementation('com.github.GTNewHorizons:GT5-Unofficial:5.09.51.400:dev') {exclude module: "Baubles" }
-    implementation('com.github.GTNewHorizons:ForbiddenMagic:0.8.0-GTNH:dev') {exclude module: "Baubles" }
+    implementation('com.github.GTNewHorizons:GT5-Unofficial:5.09.51.409:dev') {exclude module: "Baubles" }
+    implementation('com.github.GTNewHorizons:ForbiddenMagic:0.8.1-GTNH:dev') {exclude module: "Baubles" }
     implementation('com.github.GTNewHorizons:twilightforest:2.7.8:dev')
     implementation('thaumcraft:Thaumcraft:1.7.10-4.2.3.5:dev') {transitive=false}
     implementation('com.github.GTNewHorizons:NotEnoughItems:2.7.66-GTNH:dev')
 
     compile('com.github.GTNewHorizons:DummyCore:1.19.0:dev')
     compileOnly('com.github.GTNewHorizons:ForgeMultipart:1.6.7:dev') {transitive=false}
-    compileOnly('com.github.GTNewHorizons:Botania:1.12.18-GTNH:api') {transitive=false}
+    compileOnly('com.github.GTNewHorizons:Botania:1.12.21-GTNH:api') {transitive=false}
     compileOnly('com.github.GTNewHorizons:CraftTweaker:3.4.2:dev') {transitive=false}
     compileOnly('com.github.GTNewHorizons:ZenScript:1.0.0-GTNH') {transitive=false}
     compileOnly('com.github.GTNewHorizons:MagicBees:2.9.4-GTNH:api') {transitive=false}
-    compileOnly('com.github.GTNewHorizons:TinkersConstruct:1.13.43-GTNH:dev') {transitive=false}
+    compileOnly('com.github.GTNewHorizons:TinkersConstruct:1.13.46-GTNH:dev') {transitive=false}
     compileOnly(deobfCurse('enviromine-230236:2269710'))
 
     runtimeOnly('com.github.GTNewHorizons:CodeChickenCore:1.4.3:dev')

commit 7c8e3ceebc6e454e49f097f77a749b195449bfc7

fix server crash introduced by #79

src/main/java/witchinggadgets/common/items/armor/ItemPrimordialArmor.java
@@ -1,7 +1,5 @@
 package witchinggadgets.common.items.armor;
 
-import static taintedmagic.common.items.equipment.ItemVoidwalkerBoots.sashBuff;
-
 import java.util.List;
 
 import net.minecraft.block.material.Material;
@@ -39,6 +37,7 @@ import cpw.mods.fml.relauncher.SideOnly;
 import gregtech.api.hazards.Hazard;
 import gregtech.api.hazards.IHazardProtector;
 import taintedmagic.api.IVoidwalker;
+import taintedmagic.common.items.equipment.ItemVoidwalkerBoots;
 import thaumcraft.api.IRunicArmor;
 import thaumcraft.api.IVisDiscountGear;
 import thaumcraft.api.IWarpingGear;
@@ -56,6 +55,7 @@ import witchinggadgets.common.WGModCompat;
 import witchinggadgets.common.items.interfaces.IItemEvent;
 import witchinggadgets.common.items.tools.IPrimordialGear;
 import witchinggadgets.common.util.Lib;
+import witchinggadgets.mixins.early.minecraft.EntityLivingBaseAccessor;
 
 @Optional.InterfaceList({ @Optional.Interface(iface = "taintedmagic.api.IVoidwalker", modid = "TaintedMagic"),
         @Optional.Interface(iface = "thaumicboots.api.IBoots", modid = "thaumicboots"),
@@ -296,7 +296,7 @@ public class ItemPrimordialArmor extends ItemFortressArmor implements IPrimordia
             if (player.onGround || player.capabilities.isFlying || player.isOnLadder()) {
 
                 if (WGModCompat.loaded_TaintedMagic) {
-                    bonus += sashBuff(player);
+                    bonus += ItemVoidwalkerBoots.sashBuff(player);
                 }
                 bonus *= speedMod;
                 if (WitchingGadgets.isBootsActive) {
@@ -327,7 +327,7 @@ public class ItemPrimordialArmor extends ItemFortressArmor implements IPrimordia
             if (player.moveStrafing != 0.0) {
                 player.moveFlying(player.moveStrafing, 0.0F, bonus);
             }
-            boolean jumping = Minecraft.getMinecraft().gameSettings.keyBindJump.getIsKeyPressed();
+            boolean jumping = ((EntityLivingBaseAccessor)player).getIsJumping();
             boolean sneaking = player.isSneaking();
             if (sneaking && !jumping && !player.onGround) {
                 player.motionY -= bonus;
@@ -543,7 +543,6 @@ public class ItemPrimordialArmor extends ItemFortressArmor implements IPrimordia
                 if (i == 4) ++modescounter[3];
                 if (i == 5) ++modescounter[4];
                 if (i == 6) ++modescounter[5];
-                if (i == 7) ++modescounter[6];
             }
 
             switch (getAbility(stack)) {

src/main/java/witchinggadgets/core/EarlyMixinLoader.java
@@ -21,6 +21,7 @@ public class EarlyMixinLoader implements IEarlyMixinLoader, IFMLLoadingPlugin {
     public List<String> getMixins(Set<String> loadedCoreMods) {
         final List<String> mixins = new ArrayList<>();
         mixins.add("minecraft.EntityLivingAccessor");
+        mixins.add("minecraft.EntityLivingBaseAccessor");
         mixins.add("minecraft.MixinEnchantmentHelper");
         mixins.add("minecraft.MixinEntityLivingBase");
         mixins.add("minecraft.PotionEffectAccessor");

src/main/java/witchinggadgets/mixins/early/minecraft/EntityLivingBaseAccessor.java
@@ -1,0 +1,12 @@
+package witchinggadgets.mixins.early.minecraft;
+
+import net.minecraft.entity.EntityLivingBase;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(EntityLivingBase.class)
+public interface EntityLivingBaseAccessor {
+
+    @Accessor
+    boolean getIsJumping();
+}