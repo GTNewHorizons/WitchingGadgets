963c230e087c9b7c3103868cb9025a8914dc66ae
Andrei Hava <andrei@omnilium.com>
2025-12-30 10:20:28 +0200

    Don't add unnecessary aspects tag to labels in aspect library

--- src/main/java/witchinggadgets/common/gui/ContainerLabelLibrary.java
+++ src/main/java/witchinggadgets/common/gui/ContainerLabelLibrary.java
@@ -1,5 +1,7 @@
 package witchinggadgets.common.gui;
 
+import javax.annotation.Nullable;
+
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.inventory.Container;
@@ -13,6 +15,7 @@ import witchinggadgets.common.blocks.tiles.TileEntityLabelLibrary;
 
 public class ContainerLabelLibrary extends Container {
 
+    private static final String TAG_ASPECTS = "Aspects";
     private static final int LABEL_INPUT_SLOT = 0;
     private static final int LABEL_OUTPUT_SLOT = 1;
     private static final int SLOT_COUNT = 2;
@@ -84,7 +87,9 @@ public class ContainerLabelLibrary extends Container {
             Slot labelSlot = inventorySlots.get(LABEL_INPUT_SLOT);
             ItemStack labels = labelSlot.getStack();
             if (labels == null) {
-                ((IEssentiaContainerItem) clickedItem.getItem()).setAspects(clickedItem, new AspectList());
+                if (clickedItem.hasTagCompound() && clickedItem.stackTagCompound.hasKey(TAG_ASPECTS)) {
+                    clickedItem.stackTagCompound.removeTag(TAG_ASPECTS);
+                }
                 labelSlot.putStack(clickedItem);
                 clickedSlot.decrStackSize(clickedItem.stackSize);
             } else {
@@ -100,17 +105,19 @@ public class ContainerLabelLibrary extends Container {
         }
     }
 
-    private ItemStack transferLabelsWithAspect(AspectList aspects) {
+    private ItemStack transferLabelsWithAspect(@Nullable AspectList aspects) {
         Slot labelSlot = inventorySlots.get(LABEL_INPUT_SLOT);
         ItemStack labels = labelSlot.getStack();
         ItemStack returnStack = null;
         if (labelSlot.getHasStack()) {
-            // Set the labels to the given aspect (Empty list is none)
-            ((IEssentiaContainerItem) labels.getItem()).setAspects(labels, aspects);
+            // Set the labels to the given aspect if one is given
+            if (aspects != null) ((IEssentiaContainerItem) labels.getItem()).setAspects(labels, aspects);
             returnStack = labels.copy();
             if (!this.mergeItemStack(labels, SLOT_COUNT, this.inventorySlots.size(), true)) {
                 // Set un-transferred labels back to no aspects
-                ((IEssentiaContainerItem) labels.getItem()).setAspects(labels, new AspectList());
+                if (labels.hasTagCompound() && labels.stackTagCompound.hasKey(TAG_ASPECTS)) {
+                    labels.stackTagCompound.removeTag(TAG_ASPECTS);
+                }
                 return null;
             }
             if (labels.stackSize == 0) {
@@ -119,7 +126,9 @@ public class ContainerLabelLibrary extends Container {
                 labelSlot.onSlotChanged();
             }
             // Set un-transferred labels back to no aspects
-            ((IEssentiaContainerItem) labels.getItem()).setAspects(labels, new AspectList());
+            if (labels.hasTagCompound() && labels.stackTagCompound.hasKey(TAG_ASPECTS)) {
+                labels.stackTagCompound.removeTag(TAG_ASPECTS);
+            }
         }
         return returnStack;
     }
@@ -133,7 +142,9 @@ public class ContainerLabelLibrary extends Container {
             // Accept all label items no matter their aspect, and clear their aspect
             if (labelSlot.isItemValid(heldStack)) {
                 if (labels == null) {
-                    ((IEssentiaContainerItem) heldStack.getItem()).setAspects(heldStack, new AspectList());
+                    if (heldStack.hasTagCompound() && heldStack.stackTagCompound.hasKey(TAG_ASPECTS)) {
+                        heldStack.stackTagCompound.removeTag(TAG_ASPECTS);
+                    }
                     labelSlot.putStack(heldStack);
                     heldStack = null;
                 } else {
